ab7a8ec0daacf7d8dcfda91bb98ed49005c54e16
suhanit33@gmail.com <suhanit33@gmail.com>
2025-10-22 18:26:32 -0400

    Completed Lab 2 implementation - all TODO sections done

--- controller.py
+++ controller.py
@@ -15,10 +15,22 @@ class controller:
     def __init__(self, klp=0.2, klv=0.2, kli=0.2, kap=0.2, kav=0.2, kai=0.2):
         
         # TODO Part 5 and 6: Modify the below lines to test your PD, PI, and PID controller
+        # For testing P controller (current setting):
         self.PID_linear=PID_ctrl(P, klp, klv, kli, filename_="linear.csv")
         self.PID_angular=PID_ctrl(P, kap, kav, kai, filename_="angular.csv")
 
-    
+        # For testing PD controller:
+        self.PID_linear=PID_ctrl(PD, klp, klv, kli, filename_="linear.csv")
+        self.PID_angular=PID_ctrl(PD, kap, kav, kai, filename_="angular.csv")
+
+        # For testing PI controller:
+        self.PID_linear=PID_ctrl(PI, klp, klv, kli, filename_="linear.csv")
+        self.PID_angular=PID_ctrl(PI, kap, kav, kai, filename_="angular.csv")
+
+        # For testing PID controller:
+        self.PID_linear=PID_ctrl(PID, klp, klv, kli, filename_="linear.csv")
+        self.PID_angular=PID_ctrl(PID, kap, kav, kai, filename_="angular.csv")
+            
     def vel_request(self, pose, goal, status):
         
         e_lin=calculate_linear_error(pose, goal)
